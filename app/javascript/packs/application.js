250880288ed4f4981b6d9e490b62769e078f90c7
Mirza <rajwanmirza@gmail.com>
2020-04-05 19:15:18 +0600

    just Installed bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-//= require bootstrap-sprockets
+
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
@@ -16,3 +16,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap"
